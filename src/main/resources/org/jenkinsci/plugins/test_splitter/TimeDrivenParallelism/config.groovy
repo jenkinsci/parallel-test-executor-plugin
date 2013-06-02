33904d8cc6d941ad53c129f53daf3b85a5baa634
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-06-02 07:44:16 -0700

    doc improvements

--- src/main/resources/org/jenkinsci/plugins/test_splitter/TimeDrivenParallelism/config.groovy
+++ src/main/resources/org/jenkinsci/plugins/test_splitter/TimeDrivenParallelism/config.groovy
@@ -2,6 +2,6 @@ package org.jenkinsci.plugins.test_splitter.TimeDrivenParallelism
 
 def f = namespace(lib.FormTagLib)
 
-f.entry(title:"# of test minutes per execution", field:"mins") {
+f.entry(title:"Minutes per execution", field:"mins") {
     f.number()
 }
