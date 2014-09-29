95174f95aa48378d6a3b42282d4b79a51bf5dafc
Jesse Glick <jglick@cloudbees.com>
2014-09-29 01:35:39 -0700

    Passing -Djava.security.egd=file:/dev/./urandom just in case, though it does not seem to speed up class loading.

--- demo/run.sh
+++ demo/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-JENKINS_HOME=/var/lib/jenkins java -Dhudson.model.LoadStatistics.decay=0.1 -Dhudson.model.LoadStatistics.clock=1000 -jar /var/lib/jenkins/jenkins.war
+JENKINS_HOME=/var/lib/jenkins java -Djava.security.egd=file:/dev/./urandom -Dhudson.model.LoadStatistics.decay=0.1 -Dhudson.model.LoadStatistics.clock=1000 -jar /var/lib/jenkins/jenkins.war
