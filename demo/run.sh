f11a8e4f47bfe0b1fd935d4cff913e1d70c2e55f
Jesse Glick <jglick@cloudbees.com>
2015-07-30 14:56:41 -0400

    [JENKINS-24752] Noting.

--- demo/run.sh
+++ demo/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # TODO JENKINS-20108 1.585+ obsoletes java.security.egd change
-# TODO see https://github.com/jenkinsci/jenkins/pull/1433 regarding LoadStatistics overrides
+# TODO see JENKINS-24752 regarding LoadStatistics overrides
 JENKINS_HOME=/var/lib/jenkins java -Djava.security.egd=file:/dev/./urandom -Dhudson.model.LoadStatistics.decay=0.1 -Dhudson.model.LoadStatistics.clock=1000 -jar /var/lib/jenkins/jenkins.war
