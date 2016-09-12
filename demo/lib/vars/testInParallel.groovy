503dffeea3a9a91870913a9045ddb3cc99e060c8
Jesse Glick <jglick@cloudbees.com>
2016-09-12 11:58:03 -0400

    Adding block-scoped stages.

--- demo/lib/vars/testInParallel.groovy
+++ demo/lib/vars/testInParallel.groovy
@@ -2,14 +2,23 @@ def call(parallelism, inclusionsFile, exclusionsFile, results, image, prepare, r
   def splits = splitTests parallelism: parallelism, generateInclusions: true
   def branches = [:]
   for (int i = 0; i < splits.size(); i++) {
-    def split = splits[i]
-    branches["split${i}"] = {
-      docker.image(image).inside {
-        prepare()
-        writeFile file: (split.includes ? inclusionsFile : exclusionsFile), text: split.list.join("\n")
-        writeFile file: (split.includes ? exclusionsFile : inclusionsFile), text: ''
-        run()
-        junit results
+    def num = i
+    def split = splits[num]
+    branches["split${num}"] = {
+      stage("Test Section #${num + 1}") {
+        docker.image(image).inside {
+          stage('Preparation') {
+            prepare()
+            writeFile file: (split.includes ? inclusionsFile : exclusionsFile), text: split.list.join("\n")
+            writeFile file: (split.includes ? exclusionsFile : inclusionsFile), text: ''
+          }
+          stage('Main') {
+            run()
+          }
+          stage('Reporting') {
+            junit results
+          }
+        }
       }
     }
   }
