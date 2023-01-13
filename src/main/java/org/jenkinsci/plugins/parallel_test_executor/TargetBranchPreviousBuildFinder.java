cd3882c24a8f358072b5990fa38cc9c0c0bd54a0
Vincent Latombe <vincent@latombe.net>
2023-01-13 18:07:37 +0100

    Avoid using environment variables

--- src/main/java/org/jenkinsci/plugins/parallel_test_executor/TargetBranchPreviousBuildFinder.java
+++ src/main/java/org/jenkinsci/plugins/parallel_test_executor/TargetBranchPreviousBuildFinder.java
@@ -1,14 +1,13 @@
 package org.jenkinsci.plugins.parallel_test_executor;
 
-import hudson.EnvVars;
 import hudson.Extension;
 import hudson.model.ItemGroup;
 import hudson.model.Job;
 import hudson.model.Run;
 import hudson.model.TaskListener;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import jenkins.branch.Branch;
+import jenkins.scm.api.SCMHead;
+import jenkins.scm.api.mixin.ChangeRequestSCMHead;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject;
 
@@ -17,26 +16,22 @@ import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject;
  */
 @Extension(optional = true)
 public class TargetBranchPreviousBuildFinder implements PreviousBuildFinder {
-    private static final Logger LOGGER = Logger.getLogger(TargetBranchPreviousBuildFinder.class.getName());
-
     @Override
     public Run<?, ?> find(Run<?, ?> b, TaskListener listener) {
-        try {
-            EnvVars environment = b.getEnvironment(listener);
-            String changeTarget = environment.get("CHANGE_TARGET");
-            if (changeTarget != null) {
-                Job<?, ?> project = b.getParent();
-                ItemGroup itemGroup = project.getParent();
-                if (itemGroup instanceof WorkflowMultiBranchProject) {
-                    WorkflowMultiBranchProject multiBranchProject = (WorkflowMultiBranchProject) itemGroup;
-                    WorkflowJob targetBranch = multiBranchProject.getItemByBranchName(changeTarget);
-                    if (targetBranch != null) {
-                        return targetBranch.getLastBuild();
-                    }
+        Job<?, ?> project = b.getParent();
+        ItemGroup itemGroup = project.getParent();
+        if (itemGroup instanceof WorkflowMultiBranchProject) {
+            WorkflowMultiBranchProject multiBranchProject = (WorkflowMultiBranchProject) itemGroup;
+            Branch branch = multiBranchProject.getProjectFactory().getBranch((WorkflowJob) project);
+            SCMHead head = branch.getHead();
+            if (head instanceof ChangeRequestSCMHead) {
+                SCMHead target = ((ChangeRequestSCMHead) head).getTarget();
+                String targetName = target.getName();
+                WorkflowJob targetBranch = multiBranchProject.getItemByBranchName(targetName);
+                if (targetBranch != null) {
+                    return targetBranch.getLastBuild();
                 }
             }
-        } catch (IOException | InterruptedException e) {
-            LOGGER.log(Level.FINE, e, () -> "Could not look up environment variables for " + b);
         }
         return null;
     }
